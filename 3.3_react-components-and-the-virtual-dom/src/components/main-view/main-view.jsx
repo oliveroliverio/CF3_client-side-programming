fe1af65dac0bd643ff48e850ccdf87b393eee6b3
oliveroliverio <oliveroliverio@gmail.com>
2025-05-14 14:45:11 -0700

    shorthand react fragment

--- 3.3_react-components-and-the-virtual-dom/src/components/main-view/main-view.jsx
+++ 3.3_react-components-and-the-virtual-dom/src/components/main-view/main-view.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export const MainView = () => {
     return (
-        <div>
+        <>
             <div>Eloquent JavaScript</div>
             <div>Mastering JavaScript Functional Programming</div>
             <div>JavaScript: The Good Parts</div>
             <div>JavaScript: The Definitive Guide</div>
             <div>The Road to React</div>
             <button>test</button>
-        </div>
+        </>
     );
 };
