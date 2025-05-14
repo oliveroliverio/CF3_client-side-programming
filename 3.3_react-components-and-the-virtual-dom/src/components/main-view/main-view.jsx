8737950bf0d86629a728dcf8a97e37f4a28c6bfe
oliveroliverio <oliveroliverio@gmail.com>
2025-05-14 14:27:32 -0700

    Begin manual react app dev (not create-react-app): Created package.json with minimal dependencies

--- 3.3_react-components-and-the-virtual-dom/src/components/main-view/main-view.jsx
+++ 3.3_react-components-and-the-virtual-dom/src/components/main-view/main-view.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const MainView = () => {
+export const MainView = () => {
     return (
         <div>
-            <h1>Main View</h1>
+            <div>Eloquent JavaScript</div>
+            <div>Mastering JavaScript Functional Programming</div>
+            <div>JavaScript: The Good Parts</div>
+            <div>JavaScript: The Definitive Guide</div>
+            <div>The Road to React</div>
         </div>
-    )
-}
-
-export default MainView
+    );
+};
