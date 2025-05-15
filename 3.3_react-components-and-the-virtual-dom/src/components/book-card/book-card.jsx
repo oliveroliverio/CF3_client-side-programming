19d2b340d4ffe67be5b5aee067550ecf9ec4463b
oliveroliverio <oliveroliverio@gmail.com>
2025-05-14 17:01:57 -0700

    added image data

--- 3.3_react-components-and-the-virtual-dom/src/components/book-card/book-card.jsx
+++ 3.3_react-components-and-the-virtual-dom/src/components/book-card/book-card.jsx
@@ -4,6 +4,8 @@ export const BookCard = ({ bookData }) => {
     return (
         <div className="book-card">
             <h3>{bookData.title}</h3>
+            <p>{bookData.author}</p>
+            <img src={bookData.image} alt={bookData.title} />
         </div>
     );
 };
--- 3.3_react-components-and-the-virtual-dom/src/components/main-view/main-view.jsx
+++ 3.3_react-components-and-the-virtual-dom/src/components/main-view/main-view.jsx
@@ -2,35 +2,49 @@ import React from 'react'
 import { useState } from 'react';
 import { BookCard } from '../book-card/book-card';
 
-// books array
-const books = [
-    {
-        id: 1,
-        title: "Eloquent JavaScript"
-    },
-    {
-        id: 2,
-        title: "Mastering JavaScript Functional Programming"
-    },
-    {
-        id: 3,
-        title: "JavaScript: The Good Parts"
-    },
-    {
-        id: 4,
-        title: "JavaScript: The Definitive Guide"
-    },
-    {
-        id: 5,
-        title: "The Road to React"
-    }
-]
+
 
 export const MainView = () => {
-    const [booksList, setBooksList] = useState(books)
+    const [books, setBooks] = useState([
+        {
+            id: 1,
+            title: "Eloquent JavaScript",
+            image:
+                "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
+            author: "Marijn Haverbeke"
+        },
+        {
+            id: 2,
+            title: "Mastering JavaScript Functional Programming",
+            image:
+                "https://images-na.ssl-images-amazon.com/images/I/51WAikRq37L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+            author: "Federico Kereki"
+        },
+        {
+            id: 3,
+            title: "JavaScript: The Good Parts",
+            image:
+                "https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX381_BO1,204,203,200_.jpg",
+            author: "Douglas Crockford"
+        },
+        {
+            id: 4,
+            title: "JavaScript: The Definitive Guide",
+            image:
+                "https://images-na.ssl-images-amazon.com/images/I/51HbNW6RzhL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+            author: "David Flanagan"
+        },
+        {
+            id: 5,
+            title: "The Road to React",
+            image:
+                "https://images-na.ssl-images-amazon.com/images/I/41MBLi5a4jL._SX384_BO1,204,203,200_.jpg",
+            author: "Robin Wieruch"
+        }
+    ]);
     return (
         <>
-            {booksList.map((book) => (
+            {books.map((book) => (
                 <BookCard key={book.id} bookData={book} />
             ))}
             <button>test</button>
