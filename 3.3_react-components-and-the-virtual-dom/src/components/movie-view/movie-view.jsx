08279f32a6477da44ab436c39e8e1767571ae7d4
oliveroliverio <oliveroliverio@gmail.com>
2025-05-16 12:18:43 -0700

    modified movie-view component

--- 3.3_react-components-and-the-virtual-dom/src/components/movie-view/movie-view.jsx
+++ 3.3_react-components-and-the-virtual-dom/src/components/movie-view/movie-view.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
-
-export const MovieView = () => {
+const MovieView = ({ movie, onBackClick }) => {
   return (
-    <div className="movie-view">
-      <h1>Movie View</h1>
+    <div>
+      <h2>{movie.title}</h2>
+      <p>{movie.description}</p>
+      <img src={movie.poster} alt={movie.title} />
+      <p>Genre: {movie.genre}</p>
+      <p>Director: {movie.director}</p>
+      <button onClick={onBackClick}>Back</button>
     </div>
   );
 };
+
+export default MovieView;
