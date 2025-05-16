28a852c50a2f6d133fd32ddb10ef3900cd379bf9
oliveroliverio <oliveroliverio@gmail.com>
2025-05-16 11:48:17 -0700

    feat: add basic MovieCard and MovieView components with placeholder titles

---
+++ 3.3_react-components-and-the-virtual-dom/src/components/movie-card/movie-card.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const MovieCard = () => {
+  return (
+    <div className="movie-card">
+      <h1>Movie Card</h1>
+    </div>
+  );
+};
---
+++ 3.3_react-components-and-the-virtual-dom/src/components/movie-view/movie-view.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const MovieView = () => {
+  return (
+    <div className="movie-view">
+      <h1>Movie View</h1>
+    </div>
+  );
+};
