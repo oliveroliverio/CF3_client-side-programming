099614d7cf081e05cc72d267311baf9761e1a4d5
oliveroliverio <oliveroliverio@gmail.com>
2025-05-18 11:40:54 -0700

    testing update state by updating date

--- 3.4_React-Lifecycle-Methods/src/ClassComponent.js
+++ 3.4_React-Lifecycle-Methods/src/ClassComponent.js
@@ -4,7 +4,7 @@ class ClassComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // You'll add state here during your practice
+      currentDate: new Date()
     };
   }
 
@@ -28,6 +28,9 @@ class ClassComponent extends Component {
       <div>
         <h2>Class Component</h2>
         <p>This is a basic class component with lifecycle methods</p>
+        <p>Current Date: {this.state.currentDate.toLocaleDateString()}</p>
+        {/* Add a button to change the date to 5 days from now */}
+        <button onClick={() => this.setState({ currentDate: new Date(this.state.currentDate.getTime() + 5 * 24 * 60 * 60 * 1000) })}>Update Date</button>
       </div>
     );
   }
